0f0532043139c0fc8e58161a715a8ada3e08b08f
cy-moi <congyao119@gmail.com>
2022-12-23 00:05:53 +0100

    update styles

--- src/view/header.tsx
+++ src/view/header.tsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import './styles.css';
 export default function Header() {
+  const [position, setPosition] = React.useState<String>('');
 
-    const [position, setPosition] = React.useState<String>("");
-
-    return (
-        <div className='header'>
-            <div>
-                <button onClick={() => {setPosition("puzzle")}}>Puzzle</button>
-            </div>
-            <div>
-                <button onClick={() => {setPosition("puzzle")}}>About</button>
-            </div>
-            <div>
-                <button onClick={() => {setPosition("puzzle")}}>Contents</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="header">
+      <div
+        className='btn'
+        onClick={() => {
+          console.log('???');
+          setPosition('puzzle');
+        }}
+      >
+        Puzzle
+      </div>
+      <div
+        className='btn'
+        onClick={() => {
+          console.log('???');
+          setPosition('Concept');
+        }}
+      >
+        Concept
+      </div>
+      <div
+        className='btn'
+        onClick={() => {
+          console.log('???');
+          setPosition('About');
+        }}
+      >
+        About Us
+      </div>
+    </div>
+  );
 }
